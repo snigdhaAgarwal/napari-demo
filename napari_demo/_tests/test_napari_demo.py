f7028bf121417141d668960681d5d1bbb64b225d
ziyang <zliu@chanzuckerbreg.com>
2021-01-27 10:51:10 -0800

    fix test

--- napari_demo/_tests/test_napari_demo.py
+++ napari_demo/_tests/test_napari_demo.py
@@ -1,30 +1,18 @@
 import numpy as np
-from napari_demo import napari_get_reader
+from napari_demo import napari_experimental_provide_function
+from napari_demo.napari_demo import Operation
 
 
-# tmp_path is a pytest fixture
-def test_reader(tmp_path):
-    """An example of how you might test your plugin."""
+def test_function():
+    function = napari_experimental_provide_function()
+    assert callable(function)
 
-    # write some fake data using your supported file format
-    my_test_file = str(tmp_path / "myfile.npy")
-    original_data = np.random.rand(20, 20)
-    np.save(my_test_file, original_data)
-
-    # try to read it back in
-    reader = napari_get_reader(my_test_file)
-    assert callable(reader)
+    # load all files into array
+    arrays = np.ones((4, 4))
+    # stack arrays into single array
+    data = np.squeeze(np.stack(arrays))
 
     # make sure we're delivering the right format
-    layer_data_list = reader(my_test_file)
-    assert isinstance(layer_data_list, list) and len(layer_data_list) > 0
-    layer_data_tuple = layer_data_list[0]
-    assert isinstance(layer_data_tuple, tuple) and len(layer_data_tuple) > 0
-
-    # make sure it's the same as it started
-    np.testing.assert_allclose(original_data, layer_data_tuple[0])
-
-
-def test_get_reader_pass():
-    reader = napari_get_reader("fake.file")
-    assert reader is None
+    layer_data = function(data, Operation.subtract, data)
+    assert len(layer_data) == 4
+    assert (layer_data == np.zeros((4, 4))).all()

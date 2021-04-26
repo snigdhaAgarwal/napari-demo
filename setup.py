c2ee306d596dc643c33c6e54c0b648a053f0a213
ziyang <zliu@chanzuckerbreg.com>
2021-04-26 14:43:23 -0700

    add project url

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(
     install_requires=requirements,
     use_scm_version=use_scm,
     setup_requires=['setuptools_scm'],
+    project_urls={'Source Code': 'https://github.com/ziyangczi/napari-demo'},
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
